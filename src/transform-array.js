7c12893340440fd0a53ce6cfa8baf987c6f99671
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2020-09-10 23:06:55 +0300

    feat: implement "transform-array" decision

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  if (!Array.isArray(arr)) throw new Error();
+
+  let newArr = [];
+  let discarded = [];
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === "--discard-next") {
+      discarded.push(++i);
+    }
+
+    else if (arr[i] === "--double-prev") {
+      if (i > 0 && discarded.includes(i - 1) == false) {
+        newArr.push(arr[i - 1]);
+      }
+    }
+
+    else if (arr[i] === "--discard-prev") {
+      if (newArr.length > 0 && discarded.includes(i - 1) == false) {
+        newArr.pop();
+        discarded.push(i - 1);
+      }
+    }
+
+    else if (arr[i] === "--double-next") {
+      if (++i < arr.length) {
+        newArr.push(arr[i]);
+        newArr.push(arr[i]);
+      }
+    }
+
+    else {
+      newArr.push(arr[i]);
+    }
+
+  }
+
+  return newArr;
+}
