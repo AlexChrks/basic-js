87b9d4be1eaca28507d621e04e02f99e8e24508e
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2020-09-12 19:28:42 +0300

    implement "Extended repeater" solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,78 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+
+  //  _______________________________________________________Requirements
+  if (options.separator == undefined) {
+    options.separator = '+';
+  }
+
+  if (options.additionSeparator == undefined) {
+    options.additionSeparator = '|';
+  }
+
+  if (typeof str != 'string') {
+    str = str + '';
+  }
+
+  if (typeof options.addition != 'string' && options.hasOwnProperty('addition')) {
+    options.addition = options.addition + '';
+  }
+
+  if (options.repeatTimes == undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes == undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  // __________________________________________________________________
+
+
+  // _______________________________________________________Addition string
+
+  let extraArr = [];
+
+  for (let i = 1; i <= options.additionRepeatTimes; i++) {
+    extraArr.push(options.addition);
+  }
+
+  let additionalString = extraArr.reduce((acc, item, index) => {
+    if (index == 0) {
+      return acc += `${item}`;
+    }
+    else {
+      return acc += `${options.additionSeparator}${item}`
+    }
+  }, '');
+
+
+  // _________________________________________________________________
+
+  // _______________________________________________________Main string
+  let mainStr = '';
+  let mainArr = [];
+
+  if (options.hasOwnProperty('addition')) {
+    mainStr = str + additionalString;
+  }
+  else {
+    mainStr = str;
+  }
+
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    mainArr.push(mainStr);
+  }
+
+  let completeString = mainArr.reduce((acc, item, index) => {
+    if (index == 0) {
+      return acc += item;
+    }
+    else {
+      return acc += options.separator + item;
+    }
+
+  }, '');
+  // _________________________________________________________________
+  return completeString;
+}
+
